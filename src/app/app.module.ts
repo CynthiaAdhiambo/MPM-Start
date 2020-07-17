cb0468c83384e2c647877868b90c3cf98df7428a
Macreen <cynthia@thehillcroftgroup.com>
2020-07-17 11:30:38 +0300

    Added  guard to product detils screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 /*
 * Components
@@ -17,18 +18,36 @@ import { ProductListComponent } from './products/product-list.component';
 */
 import { ConvertToSpacePipe } from './shared/custom-pipes/convert-to-space-pipe';
 import { StarComponent } from './shared/components/star-component/star.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { from } from 'rxjs';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductGuardGuard } from './products/product-guard.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+
+      {path: 'products', component: ProductListComponent},
+      {
+        path: 'products/:id', 
+        canActivate: [ProductGuardGuard],
+        component: ProductDetailsComponent
+      },
+      {path: 'welcome', component: WelcomeComponent},
+      {path: ' ', redirectTo: 'welcome', pathMatch:'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+    ])
     
   ],
   bootstrap: [AppComponent]
